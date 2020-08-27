b9d66ab0c7cc2263ae7ac5dceeeb5cd4d9ed83c7
syzous <33412228+syzous@users.noreply.github.com>
2020-08-27 14:42:37 +0700

    Add files via upload

--- node-server2.js
+++ node-server2.js
@@ -1,6 +1,22 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World 2');
-}).listen(8080);
+var http = require("http");
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "database-1.chmtmcyvielh.ap-southeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Ngitest0",
+  database: "studentDB",
+});
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("select * from student", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World 2");
+  })
+  .listen(8080);
